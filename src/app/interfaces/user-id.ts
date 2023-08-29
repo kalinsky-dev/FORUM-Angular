fcf34851e938f99f5eb7aaa77a500600efbce3e9
Kalin Hristov <kalin0169@gmail.com>
2023-08-29 12:56:52 +0300

    Create user-id.ts

---
+++ src/app/interfaces/user-id.ts
@@ -0,0 +1,12 @@
+export interface UserId {
+  "_id": string,
+  "themes": string[];
+  "posts": string[];
+  "tel": string;
+  "email": string;
+  "username": string;
+  "password": string;
+  "created_at": string;
+  "updatedAt": string;
+  "__v": number;
+}
