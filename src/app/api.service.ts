550b308ef4b696a06390d3b4d42dc54c119601d7
Kalin Hristov <kalin0169@gmail.com>
2023-08-29 11:14:34 +0300

    Use http in my Api Service to fetch the Data!

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getThemes() { }
   getPosts() { }
