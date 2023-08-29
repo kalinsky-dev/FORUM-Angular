5b0b44456af3b6f48cf4d2bd3eb0966a3562b007
Kalin Hristov <kalin0169@gmail.com>
2023-08-29 14:33:23 +0300

    Posts-List Component render the correct Data coming from the Rest-Api!

--- src/app/posts-list/posts-list.component.ts
+++ src/app/posts-list/posts-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Post } from '../interfaces/posts';
 
 @Component({
   selector: 'app-posts-list',
@@ -8,11 +9,27 @@ import { ApiService } from '../api.service';
 })
 export class PostsListComponent implements OnInit {
 
+  postsList: Post[] = [];
+  isLoading: boolean = true;
+  // thereAreNoPosts: boolean = false;
+
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
-    this.apiService.getPosts(5).subscribe(posts => {
-      console.log({posts});
+    this.apiService.getPosts(5).subscribe({
+      next: (themes) => {
+        this.postsList = themes;
+
+        // if (this.postsList.length === 0) {
+        //   this.thereAreNoPosts = true;
+        // }
+
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.isLoading = false;
+        console.error(`Error: ${err}`);
+      },
     });
   }
 
