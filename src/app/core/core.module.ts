6d7d0053fd509200525802dcfa20ee4563e08ebe
Kalin Hristov <kalin0169@gmail.com>
2023-08-29 10:55:20 +0300

    Export Header and Footer Components form the Core Module to be visible in App

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -12,6 +12,11 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     CommonModule
+  ],
+  // Note: tells which components to be exported from this module
+  exports: [
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }
