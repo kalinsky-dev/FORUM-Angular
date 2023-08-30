aaad57cafbe9763c69c97b78b00937e51e2488fa
Kalin Hristov <kalin0169@gmail.com>
2023-08-30 11:44:18 +0300

    Add Guard for New Theme Component.

--- src/app/theme/theme-routing.module.ts
+++ src/app/theme/theme-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from '../main/main.component';
 import { NewThemeComponent } from './new-theme/new-theme.component';
 import { CurrentThemeComponent } from './current-theme/current-theme.component';
+import { AuthActivate } from '../core/guards/auth.activate';
 
 const routes: Routes = [
   {
@@ -21,7 +22,8 @@ const routes: Routes = [
   },
   {
     path: 'add-theme',
-    component: NewThemeComponent
+    component: NewThemeComponent,
+    canActivate: [AuthActivate],
   }
 ];
 
