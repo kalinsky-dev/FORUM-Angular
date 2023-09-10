26b76085cb26c6dc92536506cc1b764ddd9d59e5
Kalin Hristov <kalin0169@gmail.com>
2023-09-10 16:44:57 +0300

    Import FormsModule in ThemeModule
    Import FormsModule in ThemeModule to use Template Driven Forms

--- src/app/theme/theme.module.ts
+++ src/app/theme/theme.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NewThemeComponent } from './new-theme/new-theme.component';
 import { CurrentThemeComponent } from './current-theme/current-theme.component';
 import { ThemeRoutingModule } from './theme-routing.module';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -13,7 +14,8 @@ import { ThemeRoutingModule } from './theme-routing.module';
   ],
   imports: [
     CommonModule,
-    ThemeRoutingModule
+    ThemeRoutingModule,
+    FormsModule
   ]
 })
 export class ThemeModule { }
