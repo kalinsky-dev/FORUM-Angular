cd5ce697fe5cb257c637c63d2b88ba95b2cc15cf
Kalin Hristov <kalin0169@gmail.com>
2023-09-11 16:06:02 +0300

    Create Error Component for handling the Server Errors!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ErrorComponent } from './core/error/error.component';
 
 const routes: Routes = [
   {
@@ -17,6 +18,7 @@ const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./user/user.module').then((module) => module.UserModule),
   },
+  { path: 'error', component: ErrorComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
