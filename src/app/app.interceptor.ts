658572aa2a2620a75f7d0925fabc8795bc3117c7
Kalin Hristov <kalin0169@gmail.com>
2023-09-11 13:18:42 +0300

    Create App Interceptor to handle the request to the Server!

---
+++ src/app/app.interceptor.ts
@@ -0,0 +1,37 @@
+import {
+  HTTP_INTERCEPTORS,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable, Provider } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, catchError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const { apiUrl } = environment;
+
+@Injectable()
+export class AppInterceptor implements HttpInterceptor {
+  constructor(private router: Router) { }
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    if (req.url.startsWith('/api')) {
+      req = req.clone({
+        url: req.url.replace('/api', apiUrl),
+        withCredentials: true, // Cookie -> JWT
+      });
+    }
+    return next.handle(req);
+  }
+}
+
+export const appInterceptorProvider: Provider = {
+  multi: true,
+  useClass: AppInterceptor,
+  provide: HTTP_INTERCEPTORS,
+};
