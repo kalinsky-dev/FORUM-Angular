ab7594fc4125aa3e80a13ebabb3ef1d7a53e5e81
Kalin Hristov <kalin0169@gmail.com>
2023-09-12 10:26:35 +0300

    New Theme Component Works!

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -3,13 +3,21 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Theme } from './interfaces/theme';
 import { Post } from './interfaces/post';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
+  posts: any = [];
+
+  get postCollection(): string {
+    return this.posts || [];
+  }
+
+  // THEMES
   getTheme(id: string) {
     const { apiUrl } = environment;
     return this.http.get<Theme>(`${apiUrl}/themes/${id}`);
@@ -20,10 +28,17 @@ export class ApiService {
     return this.http.get<Theme[]>(`${apiUrl}/themes`);
   }
 
+  createTheme(themeName: string, postText: string) {
+    return this.http.post<Theme>('/api/themes', { themeName, postText });
+  }
+
+  // POSTS
   getPosts(limit?: number) {
     const { apiUrl } = environment;
     const limitFilter = limit ? `?limit=${limit}` : '';
 
-    return this.http.get<Post[]>(`${apiUrl}/posts${limitFilter}`);
+    return this.http
+      .get<Post[]>(`${apiUrl}/posts${limitFilter}`)
+      .pipe(tap((posts) => (this.posts = posts)));
   }
 }
--- src/app/theme/new-theme/new-theme.component.ts
+++ src/app/theme/new-theme/new-theme.component.ts
@@ -1,17 +1,27 @@
-import { Component } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
-  selector: "app-new-theme",
-  templateUrl: "./new-theme.component.html",
-  styleUrls: ["./new-theme.component.css"],
+  selector: 'app-new-theme',
+  templateUrl: './new-theme.component.html',
+  styleUrls: ['./new-theme.component.css'],
 })
 export class NewThemeComponent {
+  constructor(private apiService: ApiService, private router: Router) { }
+
+  o$ = new Observable();
+
   newThemeSubmitHandler(form: NgForm): void {
     if (form.invalid) {
       return;
     }
 
-    console.log(form.value);
+    const { themeName, postText } = form.value;
+    this.apiService.createTheme(themeName, postText).subscribe(() => {
+      this.router.navigate(['/themes']);
+    });
   }
 }
