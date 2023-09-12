0b6a6f056e15acff9b28dd43f29654d790082606
Kalin Hristov <kalin0169@gmail.com>
2023-09-12 11:27:18 +0300

    Create Slice Pipe to Cut the Theme Title in the Themes List Component!

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LoaderComponent } from "./loader/loader.component";
 import { AppEmailDirective } from "./validators/app-email.directive";
+import { SlicePipe } from './pipes/slice.pipe';
 
 @NgModule({
-  declarations: [LoaderComponent, AppEmailDirective],
+  declarations: [LoaderComponent, AppEmailDirective, SlicePipe],
   imports: [CommonModule],
-  exports: [LoaderComponent, AppEmailDirective],
+  exports: [LoaderComponent, AppEmailDirective, SlicePipe],
 })
-export class SharedModule {}
+export class SharedModule { }
 
